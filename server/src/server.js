27d178e18b26e4615893ad1d523f8bd988d12807
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-09-28 21:21:33 -0400

    file path

--- server/src/server.js
+++ server/src/server.js
@@ -3,15 +3,25 @@ import express from "express";
 import sequelize from "./config/connection.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 import routes from "./routes/index.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.static("client/dist"));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist"));
+});
+
 app.use(routes);
 
 // Force true to drop/recreate table(s) on every sync
